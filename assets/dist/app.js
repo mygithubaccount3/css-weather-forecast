a34fd45b52f3c2bdf404073b307898bfe105e975
Vlad <goncharov-vlad@ukr.net>
2019-02-20 00:59:42 +0200

    small fix

--- assets/dist/app.js
+++
@@ -1,7 +0,0 @@
-;(($) => {
-    $.getScript("assets/dist/get-data.js");
-    $.getScript("assets/dist/init-map.js");
-    $.getScript("https://maps.googleapis.com/maps/api/js?key=AIzaSyBbbG0hmgpu6gKDAxeEVPtAZlTyuZ2Cxmc&callback=initMap");
-    $.getScript("assets/dist/smooth-scrolling.js");
-    $.getScript("assets/dist/display-city.js");
-}) (jQuery);
